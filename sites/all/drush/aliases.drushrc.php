709c4012f086f5d40c0fa2c7f52a558855f223eb
Hemant Sanvaria <hsanvaria@ccistudios.com>
2014-12-02 14:36:15 -0500

    Dev commit

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,13 @@
+<?php
+
+$aliases['dev'] = array(
+	'uri'=> 'example.com',
+	'root' => '/home/denny/subdomains/dev/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'denny',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+	'ssh-options'=> '-p 37241'
+);
+
