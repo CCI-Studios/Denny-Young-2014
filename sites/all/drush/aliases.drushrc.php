cd2cb25a47f1042c8ab0dd8766ad970b4b38c396
Hemant Sanvaria <hsanvaria@ccistudios.com>
2014-12-05 11:57:57 -0500

    Added live alias to drush

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,3 +11,15 @@ $aliases['dev'] = array(
 	'ssh-options'=> '-p 37241'
 );
 
+
+$aliases['live_drupal'] = array(
+	'uri'=> 'example.com',
+	'root' => '/home/denny/subdomains/live_drupal/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'denny',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+	'ssh-options'=> '-p 37241'
+);
+
